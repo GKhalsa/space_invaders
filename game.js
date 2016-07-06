92914396a8eda7403731d26b1caee5345fabbc79
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-07-06 14:30:30 -0600

    add player constructor add body array add drawRect

--- game.js
+++ game.js
@@ -4,7 +4,7 @@
     var screen = canvas.getContext('2d');
     var gameSize = {x: canvas.width, y: canvas.height};
 
-    
+    this.bodies = [new Player(this, gameSize)];
 
     var self = this;
     var tick = function(){
@@ -19,9 +19,32 @@
     update: function(){
     },
     draw: function(screen, gameSize){
-      screen.fillRect(30,30, 40, 50);
+      for(var i = 0; i < this.bodies.length; i++){
+        drawRect(screen, this.bodies[i]);
+      }
+    }
+  };
+
+  var Player = function(game, gameSize){
+    this.game = game;
+    this.size = {x:15, y:15};
+    this.center = {x: gameSize.x/2, y: gameSize.y - this.size.x};
+  };
+
+  Player.prototype = {
+    update: function(){
 
     }
+  };
+
+  var drawRect = function(screen, body){
+    screen.fillRect(body.center.x - body.size.x / 2,
+                    body.center.y - body.size.y / 2,
+                    body.size.x, body.size.y);
+  };
+
+  var Keyboarder = function(){
+    var keyState = {};
   };
 
   window.onload = function(){
